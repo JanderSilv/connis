3b47cfd1d8e04f9be3cc50ed5966a37d1d7d2729
Jander Silva <jander.silv@outlook.com>
2022-09-08 17:27:37 -0300

    🍱 chore(assets/icons): exports used mui icons
    🍱 chore: adds recycling icon.

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,20 @@
+export {
+  AddShoppingCartRounded as AddShoppingCartIcon,
+  AttachMoneyRounded as AttachMoneyIcon,
+  BiotechRounded as BiotechIcon,
+  CenterFocusStrongRounded as CenterFocusStrongIcon,
+  FormatListBulletedRounded as FormatListBulletedIcon,
+  GrainRounded as GrainIcon,
+  InsightsRounded as InsightsIcon,
+  LocalShippingRounded as LocalShippingIcon,
+  PlusOneRounded as PlusOneIcon,
+  QuestionMarkRounded as QuestionMarkIcon,
+  RocketRounded as RocketIcon,
+  ScienceRounded as ScienceIcon,
+  SearchRounded as SearchIcon,
+  SyncRounded as SyncIcon,
+  TipsAndUpdatesRounded as TipsAndUpdatesIcon,
+  VolunteerActivismRounded as VolunteerActivismIcon,
+  WorkspacesRounded as WorkspacesIcon,
+} from '@mui/icons-material'
+export * from './recycling'
