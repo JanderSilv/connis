d1576316e9acdba22115bedf07dc096ce66451e4
Jander Silva <jander.silv@outlook.com>
2022-09-08 17:39:32 -0300

    🧑🏻‍💻 chore(models/enums): adds proposalCategory, TRL and ProposalType enums.

---
+++ src/models/enums/index.ts
@@ -0,0 +1,26 @@
+export enum ProposalCategory {
+  waste = 0,
+  disruptiveInnovation,
+  incrementalInnovation,
+  others,
+}
+
+export enum TRL {
+  trl0 = 0,
+  trl1,
+  trl2,
+  trl3,
+  trl4,
+  trl5,
+  trl6,
+  trl7,
+  trl8,
+  trl9,
+}
+
+export enum ProposalType {
+  buyOrSell = 0,
+  exchange,
+  donate,
+  research,
+}
