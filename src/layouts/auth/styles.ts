b41a75c1aace2008c4564ab115ffbbddc3710c00
Jander Silva <jander.silv@outlook.com>
2022-10-24 21:10:19 -0300

    ✨ feat(layouts/auth): implements illustration prop replacing illustrationPosition.
    📱 refactor: illustration responsiveness adjustments.

--- src/layouts/auth/styles.ts
+++ src/layouts/auth/styles.ts
@@ -1,10 +1,13 @@
 import { Box, styled } from '@mui/material'
 
 export type WrapperProps = {
-  illustrationPosition?: 'left' | 'right'
+  illustration?: {
+    position?: 'left' | 'right'
+    breakpoint?: 'sm' | 'md'
+  }
 }
 
-export const Wrapper = styled(Box)<WrapperProps>(({ theme, illustrationPosition = 'right' }) => ({
+export const Wrapper = styled(Box)<WrapperProps>(({ theme, illustration }) => ({
   width: '100%',
   minHeight: '100vh',
   padding: theme.spacing(4),
@@ -16,23 +19,21 @@ export const Wrapper = styled(Box)<WrapperProps>(({ theme, illustrationPosition 
   justifyContent: 'space-between',
   rowGap: theme.spacing(4),
 
+  [theme.breakpoints.up(illustration?.breakpoint || 'sm')]: {
+    paddingBottom: theme.spacing(4),
+    background: 'bottom / 300px no-repeat url(/assets/auth/business-make-connection.svg)',
+    backgroundPositionX: illustration?.position === 'left' ? '10%' : '95%',
+  },
+
   '&:after': {
     content: '""',
     display: 'block',
 
-    [theme.breakpoints.down('sm')]: {
+    [theme.breakpoints.down(illustration?.breakpoint || 'sm')]: {
       width: '100%',
       height: 200,
       background: 'center / contain no-repeat url(/assets/auth/business-make-connection.svg)',
     },
-  },
-
-  [theme.breakpoints.up('sm')]: {
-    paddingBottom: theme.spacing(4),
-    background:
-      illustrationPosition === 'right'
-        ? '95% bottom / 300px no-repeat url(/assets/auth/business-make-connection.svg)'
-        : '5% bottom / 300px no-repeat url(/assets/auth/business-make-connection.svg)',
   },
 }))
 Wrapper.defaultProps = {
