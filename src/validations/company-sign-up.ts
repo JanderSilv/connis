3dcba8bc49aac6b1d35c22b7d5a6e226e44da1c9
Jander Silva <jander.silv@outlook.com>
2022-10-31 17:58:15 -0300

    ♻️ refactor(validations/replaces yup by zod).

--- src/validations/company-sign-up.ts
+++ src/validations/company-sign-up.ts
@@ -1,5 +1,5 @@
 import { isValidCNPJ } from '@brazilian-utils/brazilian-utils'
-import * as yup from 'yup'
+import * as zod from 'zod'
 
 const strongPasswordRegex = /^(?=[^A-Z\s]*[A-Z])(?=[^a-z\s]*[a-z])(?=[^\d\s]*\d)(?=\w*[\W_])\S{8,}$/
 
@@ -7,36 +7,55 @@ const messages = {
   cnae: 'Código de atividade da empresa é obrigatório',
 }
 
-export const companySignUpSchemaValidation = yup.object({
-  name: yup
-    .string()
-    .required('Nome da empresa é obrigatório')
-    .min(3, 'Nome da empresa deve ter no mínimo 3 caracteres'),
-  cnpj: yup
-    .string()
-    .required('CNPJ é obrigatório')
-    .test('cnpj', 'CNPJ inválido', value => !!value && isValidCNPJ(value)),
-  email: yup.string().required('Email é obrigatório').email('Email inválido'),
-  phone: yup.string().required('Telefone é obrigatório'),
-  cnae: yup
-    .object({
-      id: yup.string().required(),
-      label: yup.string().required(),
-    })
-    .required(messages.cnae)
-    .typeError(messages.cnae),
-  password: yup
-    .string()
-    .required('Senha é obrigatório')
-    .min(8, 'Senha deve ter no mínimo 8 caracteres')
-    .matches(
-      strongPasswordRegex,
-      'A senha deve conter pelo menos uma letra maiúscula, uma letra minúscula e um número'
-    ),
-  passwordConfirmation: yup
-    .string()
-    .required('Confirmação de senha é obrigatório')
-    .oneOf([yup.ref('password')], 'A senha e a confirmação necessitam ser iguais'),
-})
+export const companySignUpValidationSchema = zod
+  .object({
+    name: zod
+      .string()
+      .min(1, 'Nome da empresa é obrigatório')
+      .min(3, 'Nome da empresa deve ter no mínimo 3 caracteres'),
+    cnpj: zod
+      .string()
+      .min(1, 'CNPJ é obrigatório')
+      .refine(value => !!value && isValidCNPJ(value), 'CNPJ inválido'),
+    email: zod.string().min(1, 'Email é obrigatório').email('Email inválido'),
+    phone: zod.string().min(1, 'Telefone é obrigatório'),
+    cnae: zod
+      .object(
+        {
+          id: zod.string().min(1, messages.cnae),
+          label: zod.string().min(1, messages.cnae),
+        },
+        {
+          required_error: messages.cnae,
+        }
+      )
+      .nullable()
+      .transform((value, ctx) => {
+        if (value == null)
+          ctx.addIssue({
+            code: zod.ZodIssueCode.custom,
+            message: messages.cnae,
+          })
+        return value ?? null
+      }),
+    password: zod
+      .string()
+      .min(1, 'Senha é obrigatório')
+      .min(8, 'Senha deve ter no mínimo 8 caracteres')
+      .regex(
+        strongPasswordRegex,
+        'A senha deve conter pelo menos uma letra maiúscula, uma letra minúscula e um número'
+      ),
+    passwordConfirmation: zod.string().min(1, 'Confirmação de senha é obrigatório'),
+  })
+  .superRefine(({ passwordConfirmation, password }, ctx) => {
+    if (passwordConfirmation !== password) {
+      ctx.addIssue({
+        code: zod.ZodIssueCode.custom,
+        message: 'A confirmação de senha não confere',
+        path: ['passwordConfirmation'],
+      })
+    }
+  })
 
-export type CompanySignUpSchema = yup.InferType<typeof companySignUpSchemaValidation>
+export type CompanySignUpSchema = zod.infer<typeof companySignUpValidationSchema>
