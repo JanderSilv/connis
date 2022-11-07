41644e1da86dc426b6d89fd3c7e6c8e1ef927e10
Jander Silva <jander.silv@outlook.com>
2022-11-07 03:19:22 -0300

    💄 refactor(layouts/app): minor styles adjustments.

--- src/layouts/app/styles.ts
+++ src/layouts/app/styles.ts
@@ -3,6 +3,9 @@ import { Box, styled } from '@mui/material'
 export const Wrapper = styled(Box)(({ theme }) => ({
   width: '100%',
   minHeight: '100vh',
+
+  display: 'flex',
+  flexDirection: 'column',
 }))
 Wrapper.defaultProps = {
   component: 'section',
