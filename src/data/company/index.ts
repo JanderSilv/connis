3ed1ab7686aa5fbeb7c18d19c5596c32574a6717
Jander Silva <jander.silv@outlook.com>
2022-11-13 23:39:54 -0300

    🗃️ chore(data/company): creates companySizes object

---
+++ src/data/company/index.ts
@@ -0,0 +1,8 @@
+import { CompanySize } from 'src/models/enums'
+
+export const companySizes = {
+  [CompanySize.micro]: 'Micro',
+  [CompanySize.small]: 'Pequeno',
+  [CompanySize.medium]: 'Médio',
+  [CompanySize.big]: 'Grande',
+}
