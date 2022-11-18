a03bfb43d6760622f1de9ec118d153ca29ea5044
Jander Silva <jander.silv@outlook.com>
2022-11-17 23:44:41 -0300

    🗃️ feat(data/proposal/type): creates the proposalTypes object.

--- src/data/proposal/proposal-type.ts
+++ src/data/proposal/proposal-type.ts
@@ -2,30 +2,37 @@ import { ProposalType } from 'src/models/enums'
 import { CardData } from 'src/components/proposal-register/cards-select'
 import { AttachMoneyIcon, BiotechIcon, SyncIcon, VolunteerActivismIcon } from 'src/assets/icons'
 
-export const proposalTypeOptions: CardData[] = [
-  {
+export const proposalTypes = {
+  [ProposalType.buyOrSell]: {
     id: ProposalType.buyOrSell,
     icon: AttachMoneyIcon,
     title: 'Compra/Venda',
     description: 'Está aberto a negociar envolvendo valores monetários',
   },
-  {
+  [ProposalType.donate]: {
     id: ProposalType.donate,
     icon: VolunteerActivismIcon,
     title: 'Doação',
     description: 'Está aberto a negociar sem o envolvimento de valores monetários',
   },
-  {
+  [ProposalType.exchange]: {
     id: ProposalType.exchange,
     icon: SyncIcon,
     title: 'Troca',
     description: 'Está aberto a negociar envolvendo troca de bens',
   },
-  {
+  [ProposalType.research]: {
     id: ProposalType.research,
     icon: BiotechIcon,
     title: 'P&D',
     description: 'Deseja realizar o processo de Pesquisa e Desenvolvimento através do ICT Cimatec.',
     solo: true,
   },
+}
+
+export const proposalTypeOptions: CardData[] = [
+  proposalTypes[ProposalType.buyOrSell],
+  proposalTypes[ProposalType.donate],
+  proposalTypes[ProposalType.exchange],
+  proposalTypes[ProposalType.research],
 ]
