898446b056f137bbc40d8c8b0b078acc1d24107d
Jander Silva <jander.silv@outlook.com>
2022-12-29 15:04:07 -0300

    ♻️ refactor(data/company): replaces medium and big prop from companySizes to 'others' prop.

--- src/data/company/index.ts
+++ src/data/company/index.ts
@@ -3,6 +3,5 @@ import { CompanySize } from 'src/models/enums'
 export const companySizes = {
   [CompanySize.micro]: 'Micro',
   [CompanySize.small]: 'Pequeno',
-  [CompanySize.medium]: 'Médio',
-  [CompanySize.big]: 'Grande',
+  [CompanySize.others]: 'Média ou Grande',
 }
