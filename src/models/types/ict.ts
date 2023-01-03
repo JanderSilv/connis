88b81f03dc9d1357c8edd29496974a8cc3e329c7
Jander Silva <jander.silv@outlook.com>
2023-01-03 19:19:58 -0300

    🏷️ chore(models/types): adds ICT types.

---
+++ src/models/types/ict.ts
@@ -0,0 +1,34 @@
+import { AnalystUser } from './user'
+
+export type ICTProject = {
+  id: number
+  title: string
+  description: string
+  image?: string
+}
+
+export type ICTLab = {
+  id: number
+  title: string
+  description: string
+  images?: string[]
+}
+
+export type ICTAddress = {
+  city: string
+  uf: string
+  cep: string
+  street: string
+  number: string
+  complement?: string
+}
+
+export type ICT = {
+  name: string
+  email: string
+  phone: string
+  projects: ICTProject[]
+  labs: ICTLab[]
+  analysts: AnalystUser[]
+  address: ICTAddress
+}
