6dfa7b096f21f441a3668d4a0bb2e769ec83a46d
Jander Silva <jander.silv@outlook.com>
2023-01-04 19:23:04 -0300

    🏷️ chore(types/next-auth.d.ts):  renames User type to AppUser to avoid conflicts with next-auth User interface.

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,9 +1,9 @@
 import NextAuth, { DefaultUser } from 'next-auth'
-import { User } from 'src/models/types'
+import { User as AppUser } from 'src/models/types'
 
 declare module 'next-auth' {
   interface Session {
-    user: User
+    user: AppUser
   }
   interface User {
     id: number
