6de84267f20d253ed4e29e78fc3555b602800c77
Jander Silva <jander.silv@outlook.com>
2023-01-07 23:55:46 -0300

    🧑🏻‍💻 feat(helpers/formatters/data): implements the longDate method.

--- src/helpers/formatters/date.ts
+++ src/helpers/formatters/date.ts
@@ -1,7 +1,17 @@
-import { formatDistanceToNow } from 'date-fns'
+import { format, formatDistanceToNow } from 'date-fns'
 
 const distanceToNow = (date: Date) => formatDistanceToNow(date, { addSuffix: true })
 
+const longDate = (date: Date) => {
+  const formattedDateArray = format(date, "dd 'de' MMMM 'de' yyyy").split(' ')
+  const monthIndex = formattedDateArray.findIndex(word => word === 'de')
+  const capitalizedMonth =
+    formattedDateArray[monthIndex + 1].charAt(0).toUpperCase() + formattedDateArray[monthIndex + 1].slice(1)
+  formattedDateArray[monthIndex + 1] = capitalizedMonth
+  return formattedDateArray.join(' ')
+}
+
 export const formatDate = {
   distanceToNow,
+  longDate,
 }
