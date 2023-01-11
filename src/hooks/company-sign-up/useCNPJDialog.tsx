84e9b5aebb60a4951c61cdc06960e5a6fa7000e6
Jander Silva <jander.silv@outlook.com>
2023-01-10 23:57:10 -0300

    🐛 fix(hooks/company-sign-up/useCNPJDialog): adds a conditional redirect to cancel button.

--- src/hooks/company-sign-up/useCNPJDialog.tsx
+++ src/hooks/company-sign-up/useCNPJDialog.tsx
@@ -1,10 +1,12 @@
 import { Dispatch, SetStateAction, useCallback, useState } from 'react'
+import { useRouter } from 'next/router'
 import { Session } from 'next-auth'
 import { signOut } from 'next-auth/react'
 import { Button, Dialog, DialogTitle, DialogContent, DialogActions, Box } from '@mui/material'
 import { useForm, UseFormReset } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 
+import { pages } from 'src/constants'
 import { useToast } from 'src/hooks/useToast'
 import { CompanyUser } from 'src/models/types'
 import { CompanySignUpSchema, CnpjSchema, cnpjValidationSchema } from 'src/validations/company-sign-up'
@@ -24,6 +26,7 @@ type Props = {
 }
 
 const CNPJDialog = ({ session, fetchedCompany, setFetchedCompany, reset }: Props) => {
+  const { replace, pathname } = useRouter()
   const { showToast } = useToast()
   const {
     register,
@@ -69,7 +72,10 @@ const CNPJDialog = ({ session, fetchedCompany, setFetchedCompany, reset }: Props
           />
         </DialogContent>
         <DialogActions>
-          <Button onClick={() => signOut()} disabled={isSubmitting}>
+          <Button
+            onClick={() => (pathname === pages.companySignUp ? replace(pages.login) : signOut())}
+            disabled={isSubmitting}
+          >
             Cancelar
           </Button>
           <Button type="submit" disabled={isSubmitting}>
