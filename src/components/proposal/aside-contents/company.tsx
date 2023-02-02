be23ed4bd62ff4fb1b83f9d6e50c10917b852cb7
Jander Silva <jander.silv@outlook.com>
2023-02-02 16:41:22 -0300

    ✨ feat(components/proposal/aside-contents/company): adds useInterestedAtProposal hook and ActionsHeader component.

--- src/components/proposal/aside-contents/company.tsx
+++ src/components/proposal/aside-contents/company.tsx
@@ -1,21 +1,44 @@
-import { Button, Stack } from '@mui/material'
-import { CompanyUser } from 'src/models/types'
+import { Button, Divider, Stack, Typography } from '@mui/material'
+import { useInterestedAtProposal } from 'src/hooks/proposal'
+import { Proposal } from 'src/models/types'
+import { ActionsHeader } from './actions-header'
 import { CompanyData } from './common'
 
-type Props = CompanyUser
+type Props = {
+  proposal: Proposal
+}
 
-export const OfferCompanyAsideContent = (company: Props) => (
-  <>
-    <CompanyData {...company} />
+export const OfferCompanyAsideContent = ({ proposal }: Props) => {
+  const { InterestedAtProposalDialog, setIsDialogOpen } = useInterestedAtProposal(proposal)
 
-    <Stack mt={3} gap={1}>
-      <Button variant="contained" color="primary" fullWidth>
-        Estou Interessado
-      </Button>
+  return (
+    <>
+      <CompanyData {...proposal.company} />
 
-      <Button variant="contained" color="warning" fullWidth>
-        Fazer contra proposta
-      </Button>
-    </Stack>
-  </>
-)
+      <Divider sx={{ mt: 2, mb: 1 }} />
+
+      <ActionsHeader>
+        <ul>
+          <li>
+            <Typography>{`"Estou Interessado" indica que você concorda com as condições da proposta e deseja fazer uma oferta de negociação.`}</Typography>
+          </li>
+          <li>
+            <Typography>{`"Fazer contra proposta" indica que você se interessou pela proposta mas gostaria de alterar algumas condições.`}</Typography>
+          </li>
+        </ul>
+      </ActionsHeader>
+
+      <Stack mt={1} gap={1}>
+        <Button variant="contained" color="primary" onClick={() => setIsDialogOpen(true)} fullWidth>
+          Estou Interessado
+        </Button>
+
+        <Button variant="contained" color="warning" fullWidth>
+          Fazer Contra Proposta
+        </Button>
+      </Stack>
+
+      <InterestedAtProposalDialog />
+    </>
+  )
+}
