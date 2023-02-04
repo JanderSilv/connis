56b59540562248c4d2f63085525a2d6af4fb85b1
Jander Silva <jander.silv@outlook.com>
2023-02-04 00:20:55 -0300

    🧑🏻‍💻 feat(helpers/formatters): adds optional options parameter to formatCurrency helper.

--- src/helpers/formatters/currency.ts
+++ src/helpers/formatters/currency.ts
@@ -1,9 +1,10 @@
-export const formatCurrency = (value?: number) =>
-  !!value
-    ? new Intl.NumberFormat('pt-BR', {
-        style: 'currency',
-        currency: 'BRL',
-      }).format(value)
-    : ''
+export const formatCurrency = (value?: number, options?: Intl.NumberFormatOptions) => {
+  if (!value) return ''
+  return new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+    ...options,
+  }).format(value)
+}
 
 export const unformatCurrency = (value?: string) => (!!value ? Number(value.replace(/[^0-9]/g, '')) : 0)
