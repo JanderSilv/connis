19146acd2950cd66adda43ab590cbc3ea048ed10
Jander Silva <jander.silv@outlook.com>
2023-02-18 22:53:36 -0300

    ✨ feat(hooks): implement the usePagination hook.

---
+++ src/hooks/usePagination.ts
@@ -0,0 +1,17 @@
+import { useCallback, useState } from 'react'
+
+export const usePagination = () => {
+  const [page, setPage] = useState(0)
+  const [rowsPerPage, setRowsPerPage] = useState(10)
+
+  const handleChangePage = useCallback((_: unknown, newPage: number) => {
+    setPage(newPage)
+  }, [])
+
+  const handleChangeRowsPerPage = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+    setRowsPerPage(parseInt(event.target.value, 10))
+    setPage(0)
+  }, [])
+
+  return { page, rowsPerPage, handleChangePage, handleChangeRowsPerPage }
+}
