1dd250962b0decf610bc255b8eef481e60d59a52
Jander Silva <jander.silv@outlook.com>
2023-02-25 11:51:25 -0300

    🗃️ feat(data/offer/status): adds the id property to offerStatus properties values.

--- src/data/offer/status.ts
+++ src/data/offer/status.ts
@@ -2,22 +2,27 @@ import { OfferStatus } from 'src/models/enums'
 
 export const offerStatus = {
   [OfferStatus.accepted]: {
+    id: OfferStatus.accepted,
     color: 'success.light',
     label: 'Aceito',
   },
   [OfferStatus.rejected]: {
+    id: OfferStatus.rejected,
     color: 'error.light',
     label: 'Cancelado',
   },
   [OfferStatus.opposed]: {
-    label: 'Contraposto',
+    id: OfferStatus.opposed,
     color: 'warning.light',
+    label: 'Contraposto',
   },
   [OfferStatus.sended]: {
+    id: OfferStatus.sended,
     color: 'info.light',
     label: 'Aguardando Resposta',
   },
   [OfferStatus.saw]: {
+    id: OfferStatus.saw,
     color: 'primary.light',
     label: 'Visualizado',
   },
