05235a65d183393993a2fbfb2e736a7c321d3696
Jander Silva <jander.silv@outlook.com>
2023-03-06 18:22:35 -0300

    ✅ test(__tests__/pages/home]: adds render home page as ICT user test.
    ✅ test: adds ThemeProvider wrapper.

--- src/__tests__/pages/home.spec.tsx
+++ src/__tests__/pages/home.spec.tsx
@@ -1,12 +1,23 @@
 import { render, screen } from '@testing-library/react'
+import { ThemeProvider } from '@mui/material'
+
 import { fakeData } from 'src/data/fake'
 import HomePage from 'src/pages/index'
+import { theme } from 'src/styles/theme'
 
 describe('Home page', () => {
   const { myProposals, recentOffers } = fakeData
-  it('should render home page', async () => {
-    render(<HomePage myProposals={myProposals} myOffers={recentOffers} />)
+  it('should render home page as company', async () => {
+    render(<HomePage myProposals={myProposals} myOffers={recentOffers} userType="company" />)
     const title = screen.getByRole('heading', { name: /Propostas Recentes/i })
+    expect(title).toBeInTheDocument()
+  })
+
+  it('should render home page as ICT', async () => {
+    render(<HomePage negotiations={myProposals} requests={myProposals} opportunities={myProposals} userType="ict" />, {
+      wrapper: ({ children }) => <ThemeProvider theme={theme}>{children}</ThemeProvider>,
+    })
+    const title = screen.getByRole('heading', { name: /Seja bem vindo ao Connis\./i })
     expect(title).toBeInTheDocument()
   })
 })
