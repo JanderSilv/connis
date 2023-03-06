da8e76c072e118954c457030b6d91c5624fd259a
Jander Silva <jander.silv@outlook.com>
2023-03-06 18:21:47 -0300

    ✅ test(__tests__/pages/proposal/[id]: adds ThemeProvider wrapper.

--- src/__tests__/pages/proposal/[id].spec.tsx
+++ src/__tests__/pages/proposal/[id].spec.tsx
@@ -1,11 +1,16 @@
 import { render, screen } from '@testing-library/react'
+import { ThemeProvider } from '@mui/material'
+
 import { fakeData } from 'src/data/fake'
 import ProposalPage from 'src/pages/proposta/[id]'
+import { theme } from 'src/styles/theme'
 
 describe('Home page', () => {
   const { proposal, myOffers } = fakeData
   it('should render proposal page', () => {
-    render(<ProposalPage proposal={proposal} offers={myOffers} />)
+    render(<ProposalPage proposal={proposal} offers={myOffers} />, {
+      wrapper: ({ children }) => <ThemeProvider theme={theme}>{children}</ThemeProvider>,
+    })
     const title = screen.getByRole('heading', { name: proposal.title })
     expect(title).toBeInTheDocument()
   })
