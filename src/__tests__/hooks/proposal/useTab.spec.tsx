4cce7031486f755e7c947016a87eaf12219d66a1
Jander Silva <jander.silv@outlook.com>
2023-03-07 23:50:42 -0300

    ✅ test(__tests__/hooks/proposal/useTab): adds tabs data to useTab hook argument.

--- src/__tests__/hooks/proposal/useTab.spec.tsx
+++ src/__tests__/hooks/proposal/useTab.spec.tsx
@@ -1,11 +1,23 @@
 import { fireEvent, render, screen, renderHook, act } from '@testing-library/react'
 
+import { BusinessIcon, DescriptionIcon } from 'src/assets/icons'
 import { useTab } from 'src/hooks/proposal'
 import { Tab, Tabs } from 'src/styles/proposal'
 
 describe('useTab', () => {
   it('should render tab', () => {
-    const { result: useTabResult } = renderHook(() => useTab())
+    const { result: useTabResult } = renderHook(() =>
+      useTab([
+        {
+          label: 'Proposta',
+          icon: DescriptionIcon,
+        },
+        {
+          label: 'Empresas Interessadas',
+          icon: BusinessIcon,
+        },
+      ])
+    )
     const { selectedTab, tabs, handleChangeTab, a11yTabProps } = useTabResult.current
     render(
       <Tabs value={selectedTab} onChange={handleChangeTab} aria-label="Abas de Navegação" centered>
@@ -20,7 +32,18 @@ describe('useTab', () => {
   })
 
   it('should change tab', () => {
-    const { result: useTabResult } = renderHook(() => useTab())
+    const { result: useTabResult } = renderHook(() =>
+      useTab([
+        {
+          label: 'Proposta',
+          icon: DescriptionIcon,
+        },
+        {
+          label: 'Empresas Interessadas',
+          icon: BusinessIcon,
+        },
+      ])
+    )
     const { selectedTab, tabs, handleChangeTab, a11yTabProps } = useTabResult.current
     render(
       <Tabs value={selectedTab} onChange={handleChangeTab} aria-label="Abas de Navegação" centered>
