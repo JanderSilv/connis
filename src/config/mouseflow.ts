4a2d46e8b9e566566c2127f8e8f440619a927fa7
Jander Silva <jander.silv@outlook.com>
2023-04-13 15:49:51 -0300

    🔧 chore(config/mouseflow): creates mouseflow setup.

---
+++ src/config/mouseflow.ts
@@ -0,0 +1,9 @@
+window['_mfq'] = window['_mfq'] || []
+
+export const initMouseflow = () => {
+  const mf = document.createElement('script')
+  mf.type = 'text/javascript'
+  mf.defer = true
+  mf.src = '//cdn.mouseflow.com/projects/2fc56308-8956-468d-a67c-76188ccaf385.js'
+  document.getElementsByTagName('head')[0].appendChild(mf)
+}
