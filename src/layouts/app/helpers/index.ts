563ac94050a2f2980a349d7bd3d37a5ce182d05a
Jander Silva <jander.silv@outlook.com>
2023-04-26 18:07:07 -0300

    ♻️ refactor(layouts/app/helpers): mounts analyst url with username instead of id.

--- src/layouts/app/helpers/index.ts
+++ src/layouts/app/helpers/index.ts
@@ -6,5 +6,5 @@ export const getProfileURL = (user?: User) => {
   if (!user) return ''
   if (checkUserIsCompany(user)) return `${pages.companyProfile}/${user.slug}`
   if (checkUserIsICT(user)) return `${pages.ictProfile}/${user.slug}`
-  else return `${pages.analystProfile}/${user.id}`
+  else return `${pages.analystProfile}/${user.username}`
 }
