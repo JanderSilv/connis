73688a9b527dd63bb79aefb7d9f86231a0986aa2
Jander Silva <jander.silv@outlook.com>
2023-05-15 14:34:37 -0300

    ✨ feat(services/user): adds login and getCurrentUser methods,

--- src/services/user/index.ts
+++ src/services/user/index.ts
@@ -4,6 +4,10 @@ import { api } from '../api'
 
 type UserSignUpSchema = Omit<User, 'id' | 'createdAt'>
 
+type LoginResponse = {
+  jwtToken: string
+}
+
 const baseUrl = '/user'
 
 const createUser = (user: UserSignUpSchema) => api.post<User>(baseUrl, user)
@@ -16,10 +20,17 @@ const validateCode = (userId: string, code: string) => api.post<boolean>(`${base
 
 const resendCode = (userId: string) => api.get<boolean>(`${baseUrl}/${userId}/phone/token`)
 
+const login = async (email: string, password: string) =>
+  api.post<LoginResponse>(`${baseUrl}/auth`, { login: email, password })
+
+const getCurrentUser = () => api.get<User>(`${baseUrl}/current`)
+
 export const userService = {
   create: createUser,
   checkEmail: checkUserEmail,
   checkUsername: checkUserUsername,
+  getCurrent: getCurrentUser,
+  login,
   validateCode,
   resendCode,
 }
