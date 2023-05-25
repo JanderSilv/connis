e89fe3dafd4213ca793c4826a3089def15b2cf0e
Jander Silva <jander.silv@outlook.com>
2023-05-25 01:25:52 -0300

    ✨ feat(components/proposal/aside-contents/owner): implements changeProposalStatus function.

--- src/components/proposal/aside-contents/owner.tsx
+++ src/components/proposal/aside-contents/owner.tsx
@@ -1,12 +1,44 @@
+import { mutate } from 'swr'
 import { Button, Stack } from '@mui/material'
+
 import { ProposalStatus } from 'src/models/enums'
 import { Proposal } from 'src/models/types'
+
+import { useConfirmDialog, useLoadingBackdrop } from 'src/contexts'
+import { useToast } from 'src/hooks'
+
 import { ProposalStatusChip } from '../chip-status'
+import { proposalService } from 'src/services'
 
 type Props = Proposal
 
 export const AsideContentOwner = (props: Props) => {
-  const { status } = props
+  const { id, status } = props
+
+  const { handleOpenConfirmDialog } = useConfirmDialog()
+  const { toggleLoading } = useLoadingBackdrop()
+  const { showToast } = useToast()
+
+  const changeProposalStatus = async (status: ProposalStatus) => {
+    const shouldCancel = status === ProposalStatus.canceled
+    const text = shouldCancel ? 'cancelada' : 'reaberta'
+
+    try {
+      toggleLoading()
+      await proposalService.update(id, { status })
+      await mutate(`${proposalService.baseUrl}/${id}`, {
+        ...props,
+        status,
+      })
+      showToast(`Proposta ${text} com sucesso`, 'success')
+    } catch (error) {
+      console.error(error)
+      showToast(`Não foi possível ${text} a proposta`, 'error')
+    } finally {
+      toggleLoading()
+    }
+  }
+
   return (
     <>
       <ProposalStatusChip status={status} size="medium" sx={{ textAlign: 'center', display: 'flex' }} />
@@ -18,9 +50,43 @@ export const AsideContentOwner = (props: Props) => {
           </Button>
         )}
 
-        <Button variant="contained" color="error" fullWidth>
-          Cancelar Proposta
-        </Button>
+        {status !== ProposalStatus.canceled && (
+          <Button
+            variant="contained"
+            color="error"
+            onClick={() =>
+              handleOpenConfirmDialog({
+                title: 'Cancelar Proposta',
+                message:
+                  'Deseja realmente cancelar a proposta? Você poderá reabri-la posteriormente e as ofertas serão mantidas.',
+                confirmButton: {
+                  onClick: () => changeProposalStatus(ProposalStatus.canceled),
+                },
+              })
+            }
+            fullWidth
+          >
+            Cancelar Proposta
+          </Button>
+        )}
+        {status !== ProposalStatus.opened && (
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() =>
+              handleOpenConfirmDialog({
+                title: 'Reabrir Proposta',
+                message: 'Deseja realmente reabrir a proposta?',
+                confirmButton: {
+                  onClick: () => changeProposalStatus(ProposalStatus.opened),
+                },
+              })
+            }
+            fullWidth
+          >
+            Reabrir Proposta
+          </Button>
+        )}
       </Stack>
     </>
   )
