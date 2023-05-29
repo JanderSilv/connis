d0fc04fe9dab24dedad4e93c6d25dd18030cc4ca
Jander Silva <jander.silv@outlook.com>
2023-05-28 23:24:54 -0300

    👽 refactor(data/offer/status): adjusts status data due api changes.

--- src/data/offer/status.ts
+++ src/data/offer/status.ts
@@ -1,6 +1,11 @@
 import { OfferStatus } from 'src/models/enums'
 
 export const offerStatus = {
+  [OfferStatus.opened]: {
+    id: OfferStatus.opened,
+    color: 'primary.light',
+    label: 'Aberto',
+  },
   [OfferStatus.accepted]: {
     id: OfferStatus.accepted,
     color: 'success.light',
@@ -10,21 +15,6 @@ export const offerStatus = {
     id: OfferStatus.rejected,
     color: 'error.light',
     label: 'Cancelado',
-  },
-  [OfferStatus.opposed]: {
-    id: OfferStatus.opposed,
-    color: 'warning.light',
-    label: 'Contraposto',
-  },
-  [OfferStatus.sended]: {
-    id: OfferStatus.sended,
-    color: 'info.light',
-    label: 'Aguardando Resposta',
-  },
-  [OfferStatus.saw]: {
-    id: OfferStatus.saw,
-    color: 'primary.light',
-    label: 'Visualizado',
   },
 }
 
