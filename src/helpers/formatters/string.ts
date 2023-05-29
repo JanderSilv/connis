2d6f799440ecf0dc9d476ce6a12e54b3db7eecf8
Jander Silva <jander.silv@outlook.com>
2023-05-28 23:23:58 -0300

    ✨ feat(helpers/formatters/string): adds capitalizeFirstLetters helper

--- src/helpers/formatters/string.ts
+++ src/helpers/formatters/string.ts
@@ -1,5 +1,19 @@
 const capitalizeFirstLetter = (str: string) => str.charAt(0).toUpperCase() + str.slice(1)
 
+const capitalizeFirstLetters = (str: string) =>
+  str
+    .trim()
+    .replace(/\s+/g, ' ')
+    .split(' ')
+    .map(w => {
+      if (w === ' ') return ''
+      const lowerWord = w.toLowerCase()
+      if (['e', 'da', 'de', 'do', 'das', 'dos', 'à', 'com'].includes(lowerWord)) return lowerWord
+      return lowerWord[0].toUpperCase() + lowerWord.slice(1)
+    })
+    .join(' ')
+
 export const formatString = {
   capitalizeFirstLetter,
+  capitalizeFirstLetters,
 }
