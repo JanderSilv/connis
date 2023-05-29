d611371ac15e93678d4b06c8dc0970390b55b518
Jander Silva <jander.silv@outlook.com>
2023-05-28 23:22:30 -0300

    🚚 refactor(models/enums): moves proposal enums to its own file.

--- src/models/enums/index.ts
+++ src/models/enums/index.ts
@@ -1,38 +1,5 @@
-export enum ProposalCategory {
-  waste = 0,
-  disruptiveInnovation,
-  incrementalInnovation,
-  others,
-}
-
-export enum TRL {
-  trl0 = 0,
-  trl1,
-  trl2,
-  trl3,
-  trl4,
-  trl5,
-  trl6,
-  trl7,
-  trl8,
-  trl9,
-}
-
-export enum ProposalType {
-  buyOrSell = 0,
-  exchange,
-  donate,
-  research,
-}
-
-export enum ProposalStatus {
-  opened = 0,
-  onNegotiation,
-  canceled,
-  finished,
-}
-
 export * from './company'
 export * from './negotiation'
 export * from './offer'
+export * from './proposal'
 export * from './user'
---
+++ src/models/enums/proposal.ts
@@ -0,0 +1,33 @@
+export enum ProposalCategory {
+  waste = 0,
+  disruptiveInnovation,
+  incrementalInnovation,
+  others,
+}
+
+export enum TRL {
+  trl1 = 1,
+  trl2,
+  trl3,
+  trl4,
+  trl5,
+  trl6,
+  trl7,
+  trl8,
+  trl9,
+}
+
+export enum ProposalType {
+  buyOrSell = 1,
+  exchange,
+  donate,
+  research,
+}
+
+export enum ProposalStatus {
+  opened = 0,
+  onNegotiation,
+  onFormalization,
+  canceled,
+  finished,
+}
