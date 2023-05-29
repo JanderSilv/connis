a4ad5570adc5c153b5d5ee326caab7ad23de2620
Jander Silva <jander.silv@outlook.com>
2023-05-28 22:40:14 -0300

    ✨ feat(hooks/proposal/useMakeAnOffer/helpers): implements getLastValue helper.

---
+++ src/hooks/proposal/useMakeAnOffer/helpers/index.ts
@@ -0,0 +1,9 @@
+import { Offer, Proposal } from 'src/models/types'
+
+type OfferProperty = keyof Offer['suggestion']
+
+export const getLastValue = (property: OfferProperty, offers: Offer[], proposal: Proposal) => {
+  const offer = offers.reverse().find(offer => !!offer.suggestion[property])
+  if (offer) return offer.suggestion[property]
+  return proposal[property]
+}
