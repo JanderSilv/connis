8b2173b9e0913d86201bfd906d40f83795d3e66e
Jander Silva <jander.silv@outlook.com>
2023-05-29 18:15:27 -0300

    🔧 chore(services/api): setups to sends the auth token to every request.

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,21 @@
 import axios from 'axios'
+import { getSession } from 'next-auth/react'
 
 export const api = axios.create({
   baseURL: `${process.env.NEXT_PUBLIC_CONNIS_API_KEY}/api`,
 })
+
+api.interceptors.request.use(
+  async config => {
+    const session = await getSession()
+
+    if (!session || !config.headers) return config
+
+    config.headers['Authorization'] = `Bearer ${session.accessToken}`
+
+    return config
+  },
+  error => {
+    return Promise.reject(error)
+  }
+)
