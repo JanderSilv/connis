d79a74695fbd09e5324b318c830dcbd7b05361f4
Jander Silva <jander.silv@outlook.com>
2023-06-04 01:49:26 -0300

    🏷️ chore(types/next-auth): adjusts next-auth entities.

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,11 +1,20 @@
 import NextAuth, { DefaultUser } from 'next-auth'
-import { OldUser as AppUser } from 'src/models/types'
+import { User as AppUser } from 'src/models/types'
 
 declare module 'next-auth' {
   interface Session {
     user: AppUser
+    accessToken: string
+    errorStatus?: number
   }
-  interface User {
-    id: AppUser['id']
+  interface User extends AppUser {
+    accessToken: string
+  }
+}
+
+declare module 'next-auth/jwt' {
+  interface JWT {
+    user?: AppUser
+    accessToken?: string
   }
 }
