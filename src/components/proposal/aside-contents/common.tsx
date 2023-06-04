1bcdf1a69c6079a8f8eca49ce198e4f9f6283509
Jander Silva <jander.silv@outlook.com>
2023-06-04 00:41:47 -0300

    👽 refactor(components/proposal/aside-contents/common): adjusts props due api model.

--- src/components/proposal/aside-contents/common.tsx
+++ src/components/proposal/aside-contents/common.tsx
@@ -1,25 +1,41 @@
 import { Typography } from '@mui/material'
+
+import { Company, ICT } from 'src/models/types'
+
 import { makeCompanyData, makeICTData } from 'src/helpers/proposal'
-import { CompanyUser, ICTUser } from 'src/models/types'
+
+import { Link, UserAvatar } from 'src/components/shared'
+
 import { KeyData } from 'src/styles/proposal'
-import { UserAvatar } from 'src/components/shared'
 import { CompanyHeader } from './styles'
+import { formatString } from 'src/helpers/formatters'
+import { pages } from 'src/constants'
 
-type Props = CompanyUser | ICTUser
+type ICTDataProps = {
+  type: 'ict'
+} & ICT
 
-export const CompanyData = (company: Props) => {
-  const { name, image, type } = company
+type CompanyDataProps = {
+  type: 'company'
+} & Company
+
+type Props = ICTDataProps | CompanyDataProps
+
+export const CompanyData = (organization: Props) => {
+  const { name, image, slug, type } = organization
   return (
     <>
-      <CompanyHeader>
-        <UserAvatar name={name} src={image} />
-        <Typography component="h2" variant="h3">
-          {name}
-        </Typography>
-      </CompanyHeader>
+      <Link href={`${type === 'company' ? pages.companyProfile : pages.ictProfile}/${slug}`} noEffect>
+        <CompanyHeader>
+          <UserAvatar name={name} src={image} />
+          <Typography component="h2" variant="h3">
+            {formatString.capitalizeFirstLetters(name)}
+          </Typography>
+        </CompanyHeader>
+      </Link>
 
       {type === 'company'
-        ? makeCompanyData(company).map(({ label, value, displayBlock }) => (
+        ? makeCompanyData(organization).map(({ label, value, displayBlock }) => (
             <KeyData key={label}>
               <Typography component="strong" display={displayBlock ? 'block' : undefined}>
                 {label}:
@@ -27,7 +43,7 @@ export const CompanyData = (company: Props) => {
               {value}
             </KeyData>
           ))
-        : makeICTData(company).map(({ label, value }) => (
+        : makeICTData(organization).map(({ label, value }) => (
             <KeyData key={label}>
               <Typography component="strong">{label}:</Typography> {value}
             </KeyData>
