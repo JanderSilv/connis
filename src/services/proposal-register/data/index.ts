aaded220000c83fd93578550b3f4a45dc1f6eaf7
Jander Silva <jander.silv@outlook.com>
2023-06-04 00:16:44 -0300

    🗨️ refactor(services/proposal-register): improves proposal prompt message.

--- src/services/proposal-register/data/index.ts
+++ src/services/proposal-register/data/index.ts
@@ -2,7 +2,7 @@ import { SuggestionsKeys } from '../models'
 
 export const suggestions: Record<SuggestionsKeys, string> = {
   proposal:
-    'Com base na descrição do projeto e da proposta abaixo, sugira em tópicos, sem marcadores nos tópicos, como melhor descrever o projeto e a proposta, com o objetivo de entender o necessário do escopo:',
+    'Com base na descrição do projeto e da proposta abaixo, sugira em tópicos, separados por quebra de linha, como melhor descrever o projeto e a proposta, com o objetivo de entender o necessário do escopo:',
   keywords:
     'Com base na descrição do projeto e da proposta, retorne algumas palavras chaves. Retorne somente as palavras-chaves (sem títulos), separadas por vírgula e sem ponto final',
   trl: 'Com base na descrição do projeto e da proposta, retorne a possível métrica TRL',
