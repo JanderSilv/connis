87ac1903d75f095e41f5500916c602a059d51eb4
Jander Silva <jander.silv@outlook.com>
2023-06-04 00:34:09 -0300

    🧑🏻‍💻 feat(components/profile/data-section/icon-data): adds optional 'order' prop.

--- src/components/profile/data-section/icon-data/index.tsx
+++ src/components/profile/data-section/icon-data/index.tsx
@@ -3,10 +3,11 @@ import { Stack, SvgIcon, Typography } from '@mui/material'
 export type IconDataProps = {
   icon: typeof SvgIcon
   children?: React.ReactNode
+  order?: number
 }
 
-export const IconData = ({ icon: Icon, children }: IconDataProps) => (
-  <Stack direction="row" spacing={1} alignItems="center" color="grey.800">
+export const IconData = ({ icon: Icon, children, order }: IconDataProps) => (
+  <Stack direction="row" spacing={1} alignItems="center" color="grey.800" order={order}>
     <Icon fontSize="small" color="inherit" />
     {typeof children === 'string' ? <Typography color="inherit">{children}</Typography> : children}
   </Stack>
