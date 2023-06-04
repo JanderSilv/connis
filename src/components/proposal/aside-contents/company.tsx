0158cd769d502379082f5d8928e494d2494cc87a
Jander Silva <jander.silv@outlook.com>
2023-06-04 00:45:02 -0300

    ✨ feat(components/proposal/aside-contents/company): implements 'Ir para negociacao' button

--- src/components/proposal/aside-contents/company.tsx
+++ src/components/proposal/aside-contents/company.tsx
@@ -1,55 +1,87 @@
+import Link from 'next/link'
+import useImmutableSWR from 'swr/immutable'
 import { Button, Divider, Stack, Typography } from '@mui/material'
 
+import { pages } from 'src/constants'
 import { useMakeAnOffer } from 'src/hooks/proposal'
 import { OfferCategory } from 'src/models/enums'
-import { Proposal } from 'src/models/types'
+import { Negotiation, Proposal } from 'src/models/types'
 
 import { ActionsHeader } from './actions-header'
 import { CompanyData } from './common'
+import { proposalService } from 'src/services'
+import { useSession } from 'next-auth/react'
 
 type Props = {
   proposal: Proposal
 }
 
-export const OfferCompanyAsideContent = ({ proposal }: Props) => {
+export const OfferCompanyAsideContent = (props: Props) => {
+  const { proposal } = props
+
   const { MakeAnOfferDialog, handleOpenMakeAnOfferDialog } = useMakeAnOffer(proposal)
+  const { data: session } = useSession()
+
+  const { data: negotiations } = useImmutableSWR(
+    [
+      `${proposalService.baseUrl}/negotiations`,
+      {
+        companyInterestedId: session?.user.companyId,
+      },
+    ],
+    proposalService.getCompanyNegotiationsFetcher
+  )
+
+  const negotiation = negotiations?.find(negotiation => negotiation.proposal.id === proposal.id)
 
   return (
     <>
-      <CompanyData {...proposal.company} />
+      <CompanyData type="company" {...proposal.company} />
 
       <Divider sx={{ mt: 2, mb: 1 }} />
 
-      <ActionsHeader>
-        <ul>
-          <li>
-            <Typography>{`"Estou Interessado" indica que você concorda com as condições da proposta e deseja fazer uma oferta de negociação.`}</Typography>
-          </li>
-          <li>
-            <Typography>{`"Fazer contra proposta" indica que você se interessou pela proposta mas gostaria de alterar algumas condições.`}</Typography>
-          </li>
-        </ul>
-      </ActionsHeader>
+      {!negotiation ? (
+        <>
+          <ActionsHeader>
+            <ul>
+              <li>
+                <Typography>{`"Estou Interessado" indica que você concorda com as condições da proposta e deseja fazer uma oferta de negociação.`}</Typography>
+              </li>
+              <li>
+                <Typography>{`"Fazer contra proposta" indica que você se interessou pela proposta mas gostaria de alterar algumas condições.`}</Typography>
+              </li>
+            </ul>
+          </ActionsHeader>
 
-      <Stack mt={1} gap={1}>
+          <Stack mt={1} gap={1}>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => handleOpenMakeAnOfferDialog(OfferCategory.default)}
+              fullWidth
+            >
+              Estou Interessado
+            </Button>
+            <Button
+              variant="contained"
+              color="warning"
+              onClick={() => handleOpenMakeAnOfferDialog(OfferCategory.counterProposal)}
+              fullWidth
+            >
+              Fazer Contra Proposta
+            </Button>
+          </Stack>
+        </>
+      ) : (
         <Button
-          variant="contained"
-          color="primary"
-          onClick={() => handleOpenMakeAnOfferDialog(OfferCategory.default)}
+          component={Link}
+          href={`${pages.proposal}/${proposal.id}/negociacao/${negotiation.id}`}
+          variant="outlined"
           fullWidth
         >
-          Estou Interessado
+          Ir para negociação
         </Button>
-
-        <Button
-          variant="contained"
-          color="warning"
-          onClick={() => handleOpenMakeAnOfferDialog(OfferCategory.counterProposal)}
-          fullWidth
-        >
-          Fazer Contra Proposta
-        </Button>
-      </Stack>
+      )}
 
       <MakeAnOfferDialog />
     </>
