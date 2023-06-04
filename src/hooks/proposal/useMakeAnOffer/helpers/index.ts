04f546e11e0d1342bb2f1e0ccc5accf689d55e8c
Jander Silva <jander.silv@outlook.com>
2023-06-04 01:15:15 -0300

    🐛 fix(hooks/proposal/useMakeAnOffer/helpers): checks if 'suggestion' exists before access its properties.

--- src/hooks/proposal/useMakeAnOffer/helpers/index.ts
+++ src/hooks/proposal/useMakeAnOffer/helpers/index.ts
@@ -3,7 +3,7 @@ import { Offer, Proposal } from 'src/models/types'
 type OfferProperty = keyof Offer['suggestion']
 
 export const getLastValue = (property: OfferProperty, offers: Offer[], proposal: Proposal) => {
-  const offer = offers.reverse().find(offer => !!offer.suggestion[property])
+  const offer = offers.reverse().find(offer => !!offer.suggestion?.[property])
   if (offer) return offer.suggestion[property]
   return proposal[property]
 }
