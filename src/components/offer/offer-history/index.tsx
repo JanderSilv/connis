dd6e36253cd49d6caf5e4136dd8795a29c4d7c30
Jander Silva <jander.silv@outlook.com>
2023-06-04 00:48:19 -0300

    👔 refactor(components/offer/offer-history): checks if offer changes is a counter proposal.
    ♻️ refactor: receives user instead of session.

--- src/components/offer/offer-history/index.tsx
+++ src/components/offer/offer-history/index.tsx
@@ -1,12 +1,11 @@
 import { useState } from 'react'
-import { Session } from 'next-auth'
 import { Step, StepButton, StepContent, Stepper, Typography } from '@mui/material'
 
 import { offerCategories } from 'src/data/offer'
 import { formatDate } from 'src/helpers/formatters'
 
 import { OfferCategory } from 'src/models/enums'
-import { Offer, Proposal } from 'src/models/types'
+import { Offer, Proposal, User } from 'src/models/types'
 
 import { OfferDataSectionOne, OfferDataSectionTwo } from 'src/components/offer'
 import { ProposalSections } from 'src/components/proposal'
@@ -17,37 +16,63 @@ import { OfferStepIconRoot } from 'src/styles/offer'
 type OfferHistoryProps = {
   proposal: Proposal
   offers: Offer[]
-  session: Session
+  user: User
 }
 
-export const OfferHistory = ({ proposal, offers, session }: OfferHistoryProps) => {
+export const OfferHistory = ({ proposal, offers, user }: OfferHistoryProps) => {
   const [activatedOfferSteps, setActivatedOfferSteps] = useState(() => ({
     ...offers.reduce((acc, offer) => ({ ...acc, [offer.id]: true }), {} as Record<string, boolean>),
   }))
   const [isProposalActive, setIsProposalActive] = useState(true)
 
+  const checkIsCounterProposal = (offer: Offer, index: number) => {
+    const { suggestion } = offer
+
+    if (!suggestion) return false
+
+    if (index === 0)
+      return (
+        proposal.budget !== suggestion.budget ||
+        proposal.trl !== suggestion.trl ||
+        proposal.goalTrl !== suggestion.goalTrl
+      )
+
+    const previousOfferSuggestion = offers[index - 1].suggestion
+
+    return (
+      previousOfferSuggestion.budget !== suggestion.budget ||
+      previousOfferSuggestion.trl !== suggestion.trl ||
+      previousOfferSuggestion.goalTrl !== suggestion.goalTrl
+    )
+  }
+
   return (
     <Stepper orientation="vertical" nonLinear>
-      {[...offers].reverse().map(offer => (
-        <Step key={offer.id} active={activatedOfferSteps[offer.id]} completed={true}>
-          <StepButton
-            icon={
-              <OfferStepIcon active={activatedOfferSteps[offer.id]} completed={true} offerCategory={offer.category} />
-            }
-            onClick={() => setActivatedOfferSteps(prev => ({ ...prev, [offer.id]: !prev[offer.id] }))}
-            sx={{ '& .MuiStepLabel-label:is(.Mui-active, .Mui-completed)': { fontWeight: '400' } }}
-          >
-            <strong>{session?.user.id === offer.user.id ? 'Você' : offer.user.name}</strong> fez uma{' '}
-            {offerCategories[offer.category].text} {formatDate.distanceToNow(new Date(offer.createdAt))}
-          </StepButton>
-          <StepContent>
-            <OfferDataSectionOne {...offer} />
-            {offer.category === OfferCategory.counterProposal && (
-              <OfferDataSectionTwo proposal={proposal} currentOffer={offer} offers={offers} />
-            )}
-          </StepContent>
-        </Step>
-      ))}
+      {offers.map((offer, index) => {
+        const offerCategory = checkIsCounterProposal(offer, index)
+          ? OfferCategory.counterProposal
+          : OfferCategory.default
+        return (
+          <Step key={offer.id} active={activatedOfferSteps[offer.id]} completed={true}>
+            <StepButton
+              icon={
+                <OfferStepIcon active={activatedOfferSteps[offer.id]} completed={true} offerCategory={offerCategory} />
+              }
+              onClick={() => setActivatedOfferSteps(prev => ({ ...prev, [offer.id]: !prev[offer.id] }))}
+              sx={{ '& .MuiStepLabel-label:is(.Mui-active, .Mui-completed)': { fontWeight: '400' } }}
+            >
+              <strong>{user.companyId === offer.company.id ? 'Você' : offer.company.name}</strong> fez uma{' '}
+              {offerCategories[offerCategory].text} {formatDate.distanceToNow(new Date(offer.createdAt))}
+            </StepButton>
+            <StepContent>
+              <OfferDataSectionOne {...offer} hideStatus={index !== 0} />
+              {offerCategory === OfferCategory.counterProposal && (
+                <OfferDataSectionTwo proposal={proposal} currentOffer={offer} offers={offers} />
+              )}
+            </StepContent>
+          </Step>
+        )
+      })}
       <Step active={isProposalActive} completed={true}>
         <StepButton
           icon={
