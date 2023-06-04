16dc9c4fdb33848374bc5f0c19605cbd8ce50a3f
Jander Silva <jander.silv@outlook.com>
2023-06-04 01:13:35 -0300

    👽 refactor(helpers/withAuth): changes 'CompanyAdmin has a company' checking due api model
    🥅 feat: catches errors and logouts user if error is code 401.

--- src/helpers/withAuth.ts
+++ src/helpers/withAuth.ts
@@ -1,10 +1,12 @@
+import { AxiosError } from 'axios'
 import { GetServerSidePropsContext, GetServerSidePropsResult } from 'next'
 import { Session } from 'next-auth'
 import { getServerSession } from 'next-auth'
+import { destroyCookie } from 'nookies'
 
 import { pages } from 'src/constants'
+import { UserType } from 'src/models/enums'
 import { authOptions } from 'src/pages/api/auth/[...nextauth]'
-import { checkUserIsCompany } from './users'
 
 interface ServerContext extends GetServerSidePropsContext {
   session: Session
@@ -15,23 +17,32 @@ export type ServerProps<P extends { [key: string]: any } = { [key: string]: any 
 ) => Promise<GetServerSidePropsResult<P>>
 
 export const withAuth = (getServerSideProps: ServerProps) => async (context: GetServerSidePropsContext) => {
-  const session = await getServerSession(context.req, context.res, authOptions)
+  try {
+    const session = await getServerSession(context.req, context.res, authOptions)
 
-  if (!session)
-    return {
-      redirect: { permanent: false, destination: pages.login },
-    }
-
-  const checkIfCompanyUserHasCnpj = () => {
-    if (checkUserIsCompany(session.user) && !session.user.cnpj)
+    if (!session)
       return {
-        redirect: { permanent: false, destination: pages.companySocialSignUp },
+        redirect: { permanent: false, destination: pages.login },
       }
+
+    const { user } = session
+
+    if (user.type === UserType.CompanyAdmin && !user.companyId)
+      return {
+        redirect: { permanent: false, destination: pages.companySignUp },
+      }
+
+    const serverSession = { ...context, session }
+
+    return await getServerSideProps(serverSession)
+  } catch (err) {
+    const error = err as AxiosError
+    console.error(error)
+    if (error.response?.status === 401) {
+      destroyCookie(context, 'next-auth.session-token')
+      return {
+        redirect: { permanent: false, destination: pages.login },
+      }
+    }
   }
-
-  checkIfCompanyUserHasCnpj()
-
-  const serverSession = { ...context, session }
-
-  return await getServerSideProps(serverSession)
 }
