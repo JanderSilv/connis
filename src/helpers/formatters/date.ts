862e5901d8a5d4eb91e574beb546c11a041ec23f
Jander Silva <jander.silv@outlook.com>
2023-06-18 21:28:24 -0300

    🩹 fix(helpers/formatters): tries to solve server/client timezone issue.
    The different timezone causes hydration error.

--- src/helpers/formatters/date.ts
+++ src/helpers/formatters/date.ts
@@ -1,9 +1,15 @@
-import { format, formatDistanceToNow } from 'date-fns'
+import { formatDistanceToNow } from 'date-fns'
+import { formatInTimeZone, utcToZonedTime } from 'date-fns-tz'
+import { ptBR } from 'date-fns/locale'
 
-const distanceToNow = (date: Date) => formatDistanceToNow(date, { addSuffix: true })
+const TIME_ZONE = 'America/Sao_Paulo'
+
+const distanceToNow = (date: Date) => formatDistanceToNow(utcToZonedTime(date, TIME_ZONE), { locale: ptBR })
 
 const longDate = (date: Date) => {
-  const formattedDateArray = format(date, "dd 'de' MMMM 'de' yyyy").split(' ')
+  const formattedDateArray = formatInTimeZone(date, TIME_ZONE, "dd 'de' MMMM 'de' yyyy", {
+    locale: ptBR,
+  }).split(' ')
   const monthIndex = formattedDateArray.findIndex(word => word === 'de')
   const capitalizedMonth =
     formattedDateArray[monthIndex + 1].charAt(0).toUpperCase() + formattedDateArray[monthIndex + 1].slice(1)
