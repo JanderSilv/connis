6829db974e2e23768e453fa821dbc5c1871976df
Jander Silva <jander.silv@outlook.com>
2023-06-18 21:49:55 -0300

    🔧 chore(services/api): implements a custom params serializer and handles 401 errors

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,25 @@
 import axios from 'axios'
-import { getSession } from 'next-auth/react'
+import { getSession, signOut } from 'next-auth/react'
+import { destroyCookie } from 'nookies'
+import { checkIsServer } from 'src/helpers/shared'
 
 export const api = axios.create({
   baseURL: `${process.env.NEXT_PUBLIC_CONNIS_API_KEY}/api`,
+  paramsSerializer: params => {
+    const searchParams = new URLSearchParams()
+
+    Object.entries(params).forEach(([key, value]) => {
+      if (Array.isArray(value)) {
+        value.forEach(item => searchParams.append(key, item))
+      } else if (typeof value === 'object') {
+        searchParams.append(key, JSON.stringify(value))
+      } else if (typeof value !== 'undefined' && value !== null) {
+        searchParams.append(key, value as string)
+      }
+    })
+
+    return searchParams.toString()
+  },
 })
 
 api.interceptors.request.use(
@@ -16,6 +33,18 @@ api.interceptors.request.use(
     return config
   },
   error => {
+    return Promise.reject(error)
+  }
+)
+
+api.interceptors.response.use(
+  response => response,
+  error => {
+    if (error.response.status === 401) {
+      if (checkIsServer()) destroyCookie(null, 'next-auth.session-token')
+      else signOut()
+    }
+
     return Promise.reject(error)
   }
 )
