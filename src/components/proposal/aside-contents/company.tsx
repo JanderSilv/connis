15110e8c3e82498cc95388f8742ef1fdadec42fe
Jander Silva <jander.silv@outlook.com>
2023-06-18 21:32:21 -0300

    👽 refactor(components/proposal/aside-contents/company): replaces  proposalService by negotiationService

--- src/components/proposal/aside-contents/company.tsx
+++ src/components/proposal/aside-contents/company.tsx
@@ -1,16 +1,17 @@
 import Link from 'next/link'
+import { useSession } from 'next-auth/react'
 import useImmutableSWR from 'swr/immutable'
 import { Button, Divider, Stack, Typography } from '@mui/material'
 
+import { OfferCategory, OfferStatus } from 'src/models/enums'
+import { Proposal } from 'src/models/types'
+
 import { pages } from 'src/constants'
 import { useMakeAnOffer } from 'src/hooks/proposal'
-import { OfferCategory } from 'src/models/enums'
-import { Negotiation, Proposal } from 'src/models/types'
+import { negotiationService } from 'src/services'
 
 import { ActionsHeader } from './actions-header'
 import { CompanyData } from './common'
-import { proposalService } from 'src/services'
-import { useSession } from 'next-auth/react'
 
 type Props = {
   proposal: Proposal
@@ -19,20 +20,24 @@ type Props = {
 export const OfferCompanyAsideContent = (props: Props) => {
   const { proposal } = props
 
-  const { MakeAnOfferDialog, handleOpenMakeAnOfferDialog } = useMakeAnOffer(proposal)
+  const { MakeAnOfferDialog, handleOpenMakeAnOfferDialog } = useMakeAnOffer(proposal, undefined, {
+    refreshOnSubmit: true,
+  })
   const { data: session } = useSession()
 
   const { data: negotiations } = useImmutableSWR(
     [
-      `${proposalService.baseUrl}/negotiations`,
+      `${negotiationService.baseUrl}/negotiations`,
       {
+        proposalId: proposal.id,
         companyInterestedId: session?.user.companyId,
       },
     ],
-    proposalService.getCompanyNegotiationsFetcher
+    negotiationService.listFetcher
   )
 
   const negotiation = negotiations?.find(negotiation => negotiation.proposal.id === proposal.id)
+  const proposalOwnerAcceptedOffer = negotiation?.offers.some(offer => offer.offerStatus === OfferStatus.accepted)
 
   return (
     <>
@@ -40,48 +45,56 @@ export const OfferCompanyAsideContent = (props: Props) => {
 
       <Divider sx={{ mt: 2, mb: 1 }} />
 
-      {!negotiation ? (
-        <>
-          <ActionsHeader>
-            <ul>
-              <li>
-                <Typography>{`"Estou Interessado" indica que você concorda com as condições da proposta e deseja fazer uma oferta de negociação.`}</Typography>
-              </li>
-              <li>
-                <Typography>{`"Fazer contra proposta" indica que você se interessou pela proposta mas gostaria de alterar algumas condições.`}</Typography>
-              </li>
-            </ul>
-          </ActionsHeader>
+      {(() => {
+        if (!negotiation)
+          return (
+            <>
+              <ActionsHeader>
+                <ul>
+                  <li>
+                    <Typography>{`"Estou Interessado" indica que você concorda com as condições da proposta e deseja fazer uma oferta de negociação.`}</Typography>
+                  </li>
+                  <li>
+                    <Typography>{`"Fazer contra proposta" indica que você se interessou pela proposta mas gostaria de alterar algumas condições.`}</Typography>
+                  </li>
+                </ul>
+              </ActionsHeader>
 
-          <Stack mt={1} gap={1}>
-            <Button
-              variant="contained"
-              color="primary"
-              onClick={() => handleOpenMakeAnOfferDialog(OfferCategory.default)}
-              fullWidth
-            >
-              Estou Interessado
-            </Button>
-            <Button
-              variant="contained"
-              color="warning"
-              onClick={() => handleOpenMakeAnOfferDialog(OfferCategory.counterProposal)}
-              fullWidth
-            >
-              Fazer Contra Proposta
-            </Button>
-          </Stack>
-        </>
-      ) : (
-        <Button
-          component={Link}
-          href={`${pages.proposal}/${proposal.id}/negociacao/${negotiation.id}`}
-          variant="outlined"
-          fullWidth
-        >
-          Ir para negociação
-        </Button>
-      )}
+              <Stack mt={1} gap={1}>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={() => handleOpenMakeAnOfferDialog(OfferCategory.default)}
+                  fullWidth
+                >
+                  Estou Interessado
+                </Button>
+                <Button
+                  variant="contained"
+                  color="warning"
+                  onClick={() => handleOpenMakeAnOfferDialog(OfferCategory.counterProposal)}
+                  fullWidth
+                >
+                  Fazer Contra Proposta
+                </Button>
+              </Stack>
+            </>
+          )
+        return proposalOwnerAcceptedOffer ? (
+          <Button
+            component={Link}
+            href={`${pages.proposal}/${proposal.id}/negociacao/${negotiation.id}`}
+            variant="outlined"
+            fullWidth
+          >
+            Ir para negociação
+          </Button>
+        ) : (
+          <Typography color="warning.light" fontWeight="bold" textAlign="center">
+            Aguardando resposta da empresa
+          </Typography>
+        )
+      })()}
 
       <MakeAnOfferDialog />
     </>
