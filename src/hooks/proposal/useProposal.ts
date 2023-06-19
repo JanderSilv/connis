bf3fca1ab468aeee2321145fd07c69631dd37802
Jander Silva <jander.silv@outlook.com>
2023-06-18 21:57:14 -0300

    👽 refactor(hooks/proposal): adjustments due api changes.

--- src/hooks/proposal/useProposal.ts
+++ src/hooks/proposal/useProposal.ts
@@ -1,13 +1,26 @@
-import { proposalService } from 'src/services'
+import useSWR from 'swr'
 import useImmutableSWR from 'swr/immutable'
+import { negotiationService, proposalService } from 'src/services'
 
 export const useProposal = (proposalId: string, userIsTheProposalOwner: boolean) => {
-  const { data: negotiations } = useImmutableSWR(
-    userIsTheProposalOwner ? `${proposalService.baseUrl}/${proposalId}/negotiations` : null,
-    proposalService.getNegotiationsFetcher
+  const { data: negotiations } = useSWR(
+    userIsTheProposalOwner
+      ? [
+          `${negotiationService.baseUrl}/negotiations`,
+          {
+            proposalId,
+          },
+        ]
+      : null,
+    negotiationService.listFetcher
+  )
+  const { data: similarProposals } = useImmutableSWR(
+    userIsTheProposalOwner ? [`${proposalService.baseUrl}/${proposalId}/similars`, proposalId] : null,
+    proposalService.getSimilarProposalsFetcher
   )
 
   return {
     negotiations,
+    similarProposals,
   }
 }
