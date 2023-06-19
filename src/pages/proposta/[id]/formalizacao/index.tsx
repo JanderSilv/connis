37dc31dbfbc733ee027108d199f83f3950a2556a
Jander Silva <jander.silv@outlook.com>
2023-06-18 21:21:03 -0300

    🚧 feat(pages/proposta/[id]/formalizacao): integrates partially the page with the api.

--- src/pages/proposta/[id]/formalizacao/index.tsx
+++ src/pages/proposta/[id]/formalizacao/index.tsx
@@ -1,7 +1,6 @@
-import { GetServerSideProps, NextPage } from 'next'
+import { NextPage } from 'next'
+import { useRouter } from 'next/navigation'
 import { Session } from 'next-auth'
-import Head from 'next/head'
-import { ParsedUrlQuery } from 'querystring'
 import {
   Box,
   Button,
@@ -14,14 +13,17 @@ import {
   useScrollTrigger,
 } from '@mui/material'
 
+import { OrderDirection, ProposalStatus } from 'src/models/enums'
+import { ChatMessage, ICTOffer, HttpResponseError, Negotiation } from 'src/models/types'
+
 import { pages } from 'src/constants'
 import { useConfirmDialog } from 'src/contexts/confirm-dialog'
 import { fakeData } from 'src/data/fake'
+import { toastHttpError } from 'src/helpers/shared'
 import { withAuth } from 'src/helpers/withAuth'
+import { useComponentIsMounted } from 'src/hooks'
 import { useProposalSession, useTab } from 'src/hooks/proposal'
-
-import { ProposalStatus } from 'src/models/enums'
-import { ChatMessage, ICTOffer, Offer, Proposal } from 'src/models/types'
+import { companyService, offerService, proposalService } from 'src/services'
 
 import { Chat, ICTOfferSection } from 'src/components/negotiation'
 import { OfferHistory } from 'src/components/offer'
@@ -31,22 +33,23 @@ import { Layout } from 'src/layouts/app'
 
 import { ChatIcon, DescriptionIcon } from 'src/assets/icons'
 import { ProposalTitle, Section, Tab, Tabs, Wrapper } from 'src/styles/proposal'
-import { useComponentIsMounted } from 'src/hooks'
 
 type NegotiationPageProps = {
-  proposal: Proposal
-  offers: Offer[]
+  negotiation: Negotiation
   session: Session
   messages: ChatMessage[]
   ictOffer?: ICTOffer
 }
 
 const NegotiationPage: NextPage<NegotiationPageProps> = props => {
-  const { proposal, offers, messages, session, ictOffer } = props
+  const { negotiation, messages, session, ictOffer } = props
+  const { proposal, offers } = negotiation
 
-  const { userIsTheProposalOwner } = useProposalSession(proposal, session)
   const isMobile = useMediaQuery<Theme>(theme => theme.breakpoints.down('md'))
   const scrollTrigger = useScrollTrigger()
+  const { replace } = useRouter()
+  const { handleOpenConfirmDialog } = useConfirmDialog()
+  const { isComponentMounted } = useComponentIsMounted(1500)
   const { tabs, selectedTab, handleChangeTab, a11yTabProps } = useTab([
     {
       label: 'Conversa',
@@ -57,18 +60,12 @@ const NegotiationPage: NextPage<NegotiationPageProps> = props => {
       icon: DescriptionIcon,
     },
   ])
-  const { handleOpenConfirmDialog } = useConfirmDialog()
-  const { isComponentMounted } = useComponentIsMounted(1500)
 
-  const documentTitle = `Negociação - ${proposal.title} - Connis`
+  const { userIsTheProposalOwner } = useProposalSession(proposal, session)
 
   return (
-    <Layout wrapperProps={{ sx: { pb: 10 } }}>
-      <Head>
-        <title>{documentTitle}</title>
-      </Head>
-
-      <ProposalTitle>Oferta</ProposalTitle>
+    <Layout wrapperProps={{ sx: { pb: 10 } }} documentTitle={`Negociação - ${proposal.title}`}>
+      <ProposalTitle>Formalização</ProposalTitle>
       <Typography component="h2" variant="h3" color="text.secondary" textAlign="center">
         {proposal.title}
       </Typography>
@@ -97,75 +94,79 @@ const NegotiationPage: NextPage<NegotiationPageProps> = props => {
 
           <Box component="aside" flex={0.4} position="relative">
             <Section sx={{ position: 'sticky', top: 32 }}>
-              {/* {userIsTheProposalOwner && (proposal as any).offerCompany ? ( */}
-              <>
-                <CompanyData {...(proposal as any).offerCompany} />
+              {userIsTheProposalOwner && !!negotiation.companyInterested ? (
+                <>
+                  <CompanyData type="company" {...negotiation.companyInterested} />
 
-                <Divider sx={{ mt: 2, mb: 1 }} />
+                  <Divider sx={{ mt: 2, mb: 1 }} />
 
-                <ActionsHeader>
-                  <ul>
-                    <li>
-                      <Typography>
-                        &lsquo;Finalizar a negociação&lsquo; significa que você chegou a um acordo com a empresa e a
-                        proposta foi resolvida.
-                      </Typography>
-                    </li>
-                    <li>
-                      <Typography>
-                        &lsquo;Cancelar a negociação&lsquo; significa que você não chegou a um acordo e poderá voltar a
-                        avaliar a oferta de outras empresas.
-                      </Typography>
-                    </li>
-                  </ul>
-                </ActionsHeader>
+                  <ActionsHeader>
+                    <ul>
+                      <li>
+                        <Typography>
+                          &lsquo;Finalizar a negociação&lsquo; significa que você chegou a um acordo com a empresa e a
+                          proposta foi resolvida.
+                        </Typography>
+                      </li>
+                      <li>
+                        <Typography>
+                          &lsquo;Cancelar a negociação&lsquo; significa que você não chegou a um acordo e poderá voltar
+                          a avaliar a oferta de outras empresas.
+                        </Typography>
+                      </li>
+                    </ul>
+                  </ActionsHeader>
 
-                <Stack mt={1} gap={1}>
-                  <Button
-                    variant="contained"
-                    onClick={() =>
-                      handleOpenConfirmDialog({
-                        title: 'Finalizar negociação',
-                        message:
-                          'Significa que você chegou a um acordo com a empresa e a proposta foi resolvida. A proposta não poderá ser reaberta.',
-                        confirmButton: {
-                          onClick: () => {
-                            // TODO: Implements the conclude negotiation
-                            console.log('Finalizar negociação')
+                  <Stack mt={1} gap={1}>
+                    <Button
+                      variant="contained"
+                      onClick={() =>
+                        handleOpenConfirmDialog({
+                          title: 'Finalizar negociação',
+                          message:
+                            'Significa que você chegou a um acordo com a empresa e a proposta foi resolvida. A proposta não poderá ser reaberta.',
+                          confirmButton: {
+                            onClick: () => {
+                              // TODO: Implements the conclude negotiation
+                              console.log('Finalizar negociação')
+                            },
                           },
-                        },
-                      })
-                    }
-                    fullWidth
-                  >
-                    Finalizar negociação
-                  </Button>
+                        })
+                      }
+                      fullWidth
+                    >
+                      Finalizar negociação
+                    </Button>
 
-                  <Button
-                    variant="outlined"
-                    color="error"
-                    onClick={() => {
-                      handleOpenConfirmDialog({
-                        title: 'Cancelar negociação',
-                        message:
-                          'Significa que você não chegou a um acordo e poderá voltar a avaliar a oferta de outras empresas.',
-                        confirmButton: {
-                          onClick: () => {
-                            // TODO: Implements the cancel negotiation
-                            console.log('Cancelar negociação')
+                    <Button
+                      variant="outlined"
+                      color="error"
+                      onClick={() => {
+                        handleOpenConfirmDialog({
+                          title: 'Cancelar negociação',
+                          message:
+                            'Significa que você não chegou a um acordo e poderá voltar a avaliar a oferta de outras empresas.',
+                          confirmButton: {
+                            onClick: async () => {
+                              try {
+                                await proposalService.rejectNegotiation(proposal.id)
+                                replace(`${pages.proposal}/${proposal.id}`)
+                              } catch (error) {
+                                toastHttpError(error)
+                              }
+                            },
                           },
-                        },
-                      })
-                    }}
-                    fullWidth
-                  >
-                    Cancelar negociação
-                  </Button>
-                </Stack>
-              </>
-              {/* ) : (
-                <CompanyData {...(proposal as any).company} />
-              )} */}
+                        })
+                      }}
+                      fullWidth
+                    >
+                      Cancelar negociação
+                    </Button>
+                  </Stack>
+                </>
+              ) : (
+                <CompanyData type="company" {...negotiation.companyProponent} />
+              )}
             </Section>
           </Box>
         </Wrapper>
@@ -173,7 +174,7 @@ const NegotiationPage: NextPage<NegotiationPageProps> = props => {
 
       <TabPanel value={selectedTab} index={1} flex={1} withGrowAnimation>
         <Wrapper maxWidth="xl">
-          <OfferHistory proposal={proposal} offers={offers} session={session} />
+          <OfferHistory proposal={proposal} offers={offers} user={session.user} />
         </Wrapper>
       </TabPanel>
 
@@ -202,33 +203,52 @@ export default NegotiationPage
 
 type Params = {
   id: string
-} & ParsedUrlQuery
+}
 
-export const getServerSideProps: GetServerSideProps = withAuth(async context => {
-  const { myOffers, proposal, messages, ictOffer } = fakeData
-
-  // TODO: Remove fake data
+export const getServerSideProps = withAuth(async context => {
+  const { messages, ictOffer } = fakeData
   const { params, session } = context
   const { id } = params as Params
 
-  // if (![proposal.company.id, proposal.offerCompany?.id].includes(session?.user.id)) return { notFound: true }
-  // if (proposal.status !== ProposalStatus.onNegotiation)
-  //   return {
-  //     redirect: {
-  //       destination: `${pages.proposal}/${id}`,
-  //       permanent: false,
-  //     },
-  //   }
+  try {
+    const { data: negotiation } = await proposalService.getAcceptedNegotiation(id)
+    const { data: offersData } = await offerService.getCompanyOffers({
+      negotiationId: negotiation.id,
+      orderBy: 'createdAt',
+      orderDirection: OrderDirection.Desc,
+    })
+    const { data: companyProponent } = await companyService.get(negotiation.companyProponent.id)
+    const { data: companyInterested } = await companyService.get(negotiation.companyInterested.id)
 
-  console.log({ id })
+    if (
+      session?.user.companyId &&
+      ![negotiation.companyProponent.id, negotiation.companyInterested?.id].includes(session?.user.companyId)
+    )
+      return { notFound: true }
+    if (negotiation.proposal.status !== ProposalStatus.onFormalization)
+      return {
+        redirect: {
+          destination: `${pages.proposal}/${id}`,
+          permanent: false,
+        },
+      }
 
-  return {
-    props: {
-      proposal,
-      offers: myOffers[0],
-      messages,
-      ictOffer,
-      session,
-    },
+    return {
+      props: {
+        negotiation: {
+          ...negotiation,
+          offers: offersData.offers,
+          companyProponent,
+          companyInterested,
+        },
+        messages,
+        ictOffer,
+        session,
+      },
+    }
+  } catch (err) {
+    const error = err as HttpResponseError
+    if (error.response?.status === 400) return { notFound: true }
+    throw err
   }
 })
