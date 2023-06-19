bb73540bc02e9265f1fac5f725e2669b0ed942b4
Jander Silva <jander.silv@outlook.com>
2023-06-18 21:14:30 -0300

    ✨ feat(pages/proposta/[id]/negociacao): integrates the company's page version with api.

--- src/pages/proposta/[id]/negociacao/[negotiationId].tsx
+++ src/pages/proposta/[id]/negociacao/[negotiationId].tsx
@@ -1,16 +1,22 @@
-import { GetServerSideProps, NextPage } from 'next'
+import { NextPage } from 'next'
+import { useRouter } from 'next/navigation'
 import { Session } from 'next-auth'
-import Head from 'next/head'
-import { ParsedUrlQuery } from 'querystring'
+import useSWR from 'swr'
+import useSWRImmutable from 'swr/immutable'
 import { Box, Button, Divider, Stack, Typography } from '@mui/material'
 
+import { pages } from 'src/constants'
+import { useLoadingBackdrop } from 'src/contexts'
 import { useConfirmDialog } from 'src/contexts/confirm-dialog'
-import { fakeData } from 'src/data/fake'
+import { toastHttpError } from 'src/helpers/shared'
+import { checkUserIsICT } from 'src/helpers/users'
 import { withAuth } from 'src/helpers/withAuth'
-import { useOfferSession } from 'src/hooks/offer'
-import { useProposalSession } from 'src/hooks/proposal'
+import { useToast } from 'src/hooks'
+import { useMakeAnOffer, useProposalSession } from 'src/hooks/proposal'
+import { negotiationService, offerService, proposalService } from 'src/services'
 
-import { Offer, Proposal } from 'src/models/types'
+import { OfferCategory, OrderDirection, ProposalStatus } from 'src/models/enums'
+import { Negotiation, Offer } from 'src/models/types'
 
 import { OfferHistory } from 'src/components/offer'
 import { ActionsHeader, CompanyData } from 'src/components/proposal'
@@ -19,68 +25,84 @@ import { Layout } from 'src/layouts/app'
 
 import { ProposalTitle, Section, Wrapper } from 'src/styles/proposal'
 
-type OfferPageProps = {
-  proposal: Proposal
+type NegotiationPageProps = {
+  negotiation: Negotiation
   offers: Offer[]
   session: Session
 }
 
-const OfferPage: NextPage<OfferPageProps> = ({ offers, proposal, session }) => {
-  const currentOffer = offers.at(-1)
+const NegotiationPage: NextPage<NegotiationPageProps> = props => {
+  const { negotiation: initialNegotiation, offers: initialOffers, session } = props
+
+  const { push, replace } = useRouter()
+  const { handleOpenConfirmDialog } = useConfirmDialog()
+  const { toggleLoading } = useLoadingBackdrop()
+  const { showToast } = useToast()
+
+  const { data: negotiation } = useSWRImmutable(
+    `${proposalService.baseUrl}/${initialNegotiation.proposal.id}/negotiations/${initialNegotiation.id}`,
+    negotiationService.getFetcher,
+    {
+      fallbackData: initialNegotiation,
+    }
+  )
+  const { data: offers, mutate } = useSWR(
+    [
+      `${offerService.companyBaseUrl}`,
+      { negotiationId: negotiation.id, orderBy: 'createdAt', orderDirection: OrderDirection.Desc },
+    ],
+    offerService.getCompanyOffersFetcher,
+    {
+      fallbackData: initialOffers,
+      revalidateIfStale: false,
+    }
+  )
+
+  const { proposal, companyInterested, companyProponent } = negotiation
 
   const { userIsTheProposalOwner } = useProposalSession(proposal, session)
-  const { userIsTheOfferOwner } = useOfferSession(currentOffer, session)
-  const { handleOpenConfirmDialog } = useConfirmDialog()
-
-  const documentTitle = `Oferta ${proposal.id} - ${proposal.title} - Connis`
+  const { MakeAnOfferDialog, handleOpenMakeAnOfferDialog } = useMakeAnOffer(proposal, offers)
+  const currentOffer = offers[0]
 
   return (
-    <Layout>
-      <Head>
-        <title>{documentTitle}</title>
-      </Head>
-
-      <ProposalTitle>Oferta</ProposalTitle>
+    <Layout documentTitle={`Negociação - ${proposal.title} - ${negotiation.id}`}>
+      <ProposalTitle>Negociação</ProposalTitle>
       <Typography component="h2" variant="h3" color="text.secondary" textAlign="center">
         {proposal.title}
       </Typography>
 
       <Wrapper maxWidth="xl">
         <Box component="main" flex={1}>
-          <OfferHistory proposal={proposal} offers={offers} session={session} />
+          <OfferHistory proposal={proposal} offers={offers} user={session.user} />
         </Box>
 
         <Box component="aside" flex={0.4} position="relative">
           <Section sx={{ mt: 6, position: 'sticky', top: 32 }}>
-            {userIsTheProposalOwner && proposal.offerCompany ? (
-              <CompanyData {...proposal.offerCompany} />
+            {userIsTheProposalOwner ? (
+              <CompanyData type="company" {...companyInterested} />
             ) : (
-              <CompanyData {...(proposal as any).company} />
+              <CompanyData type="company" {...companyProponent} />
             )}
             {!!currentOffer && (
               <>
                 <Divider sx={{ mt: 2, mb: 1 }} />
-                {userIsTheOfferOwner ? (
-                  <Typography color="warning.light" fontWeight="bold" textAlign="center">
-                    Aguardando resposta da empresa
-                  </Typography>
-                ) : (
+                {currentOffer.company.id !== session.user.companyId ? (
                   <>
                     <ActionsHeader>
                       <ul>
                         <li>
                           <Typography>
                             {userIsTheProposalOwner
-                              ? 'Iniciar a negociação significa que você concorda com os termos atuais da proposta, além de impedir que você siga com outras ofertas.'
+                              ? 'Formalizar a negociação significa concordar com os termos atuais da proposta e seguir negociação com esta empresa.'
                               : 'Aceitar a oferta significa que você concorda com os termos atuais da proposta e irá aguardar a resposta da empresa para dar início a negociação.'}
                           </Typography>
                         </li>
                         <li>
                           <Typography>Fazer uma contra proposta serve para alterar pontos da proposta.</Typography>
                         </li>
-                        <li>
+                        {/* <li>
                           <Typography>Recusar a oferta significa não seguir em negociação com esta empresa.</Typography>
-                        </li>
+                        </li> */}
                       </ul>
                     </ActionsHeader>
 
@@ -91,20 +113,27 @@ const OfferPage: NextPage<OfferPageProps> = ({ offers, proposal, session }) => {
                           color="success"
                           onClick={() => {
                             handleOpenConfirmDialog({
-                              title: 'Iniciar negociação',
+                              title: 'Formalizar Negociação',
                               message:
-                                'Iniciar a negociação significa que você concorda com os termos atuais da proposta, além de impedir que você siga com outras ofertas.',
+                                'Formalizar a negociação significa que você concorda com os termos atuais da proposta, além de impedir que você siga com outras negociações.',
                               confirmButton: {
-                                onClick: () => {
-                                  // TODO: Implements the start negotiation
-                                  console.log('Iniciar negociação')
+                                onClick: async () => {
+                                  try {
+                                    toggleLoading()
+                                    await proposalService.acceptNegotiation(proposal.id, negotiation.id)
+                                    push(`${pages.proposal}/${proposal.id}${pages.formalization}`)
+                                  } catch (error) {
+                                    toastHttpError(error)
+                                  } finally {
+                                    toggleLoading()
+                                  }
                                 },
                               },
                             })
                           }}
                           fullWidth
                         >
-                          Iniciar Negociação
+                          Formalizar Negociação
                         </Button>
                       ) : (
                         <Button
@@ -114,11 +143,26 @@ const OfferPage: NextPage<OfferPageProps> = ({ offers, proposal, session }) => {
                             handleOpenConfirmDialog({
                               title: 'Aceitar oferta',
                               message:
-                                'Aceitar a oferta significa que você concorda com os termos atuais da proposta e irá aguardar a resposta da empresa para dar início a negociação.',
+                                'Aceitar a oferta significa que você concorda com os termos atuais da proposta e irá aguardar a resposta da empresa para formalizar a negociação.',
                               confirmButton: {
-                                onClick: () => {
-                                  // TODO: Implements the accept offer
-                                  console.log('Aceitar oferta')
+                                onClick: async () => {
+                                  try {
+                                    toggleLoading()
+                                    await offerService.makeCompanyOffer({
+                                      negotiationId: negotiation.id,
+                                      userProponentId: session.user.id,
+                                      companyProponentId: session.user.companyId!,
+                                      description:
+                                        'Mensagem automática: A empresa concorda com os termos atuais da proposta e deseja prosseguir a negociação.',
+                                      proposalType: currentOffer.proposalType,
+                                    })
+                                    showToast('Oferta enviada com sucesso', 'success')
+                                    mutate()
+                                  } catch (error) {
+                                    toastHttpError(error)
+                                  } finally {
+                                    toggleLoading()
+                                  }
                                 },
                               },
                             })
@@ -128,10 +172,15 @@ const OfferPage: NextPage<OfferPageProps> = ({ offers, proposal, session }) => {
                           Aceitar Oferta
                         </Button>
                       )}
-                      <Button variant="contained" color="warning" fullWidth>
+                      <Button
+                        variant="contained"
+                        color="warning"
+                        onClick={() => handleOpenMakeAnOfferDialog(OfferCategory.counterProposal)}
+                        fullWidth
+                      >
                         Fazer Contra Proposta
                       </Button>
-                      <Button
+                      {/* <Button
                         variant="contained"
                         color="error"
                         onClick={() => {
@@ -139,9 +188,13 @@ const OfferPage: NextPage<OfferPageProps> = ({ offers, proposal, session }) => {
                             title: 'Recusar oferta',
                             message: 'Recusar a oferta significa não seguir em negociação com esta empresa.',
                             confirmButton: {
-                              onClick: () => {
-                                // TODO: Implements the reject offer
-                                console.log('Recusar oferta')
+                              onClick: async () => {
+                                try {
+                                  await proposalService.rejectNegotiation(proposal.id, negotiation.id)
+                                  replace(`${pages.proposal}/${proposal.id}`)
+                                } catch (error) {
+                                  toastHttpError(error)
+                                }
                               },
                             },
                           })
@@ -149,9 +202,13 @@ const OfferPage: NextPage<OfferPageProps> = ({ offers, proposal, session }) => {
                         fullWidth
                       >
                         Recusar Oferta
-                      </Button>
+                      </Button> */}
                     </Stack>
                   </>
+                ) : (
+                  <Typography color="warning.light" fontWeight="bold" textAlign="center">
+                    Aguardando resposta da empresa
+                  </Typography>
                 )}
               </>
             )}
@@ -159,34 +216,54 @@ const OfferPage: NextPage<OfferPageProps> = ({ offers, proposal, session }) => {
         </Box>
       </Wrapper>
 
+      <MakeAnOfferDialog />
       <ScrollTop />
     </Layout>
   )
 }
 
-export default OfferPage
+export default NegotiationPage
 
 type Params = {
   id: string
-  offerId: string
-} & ParsedUrlQuery
+  negotiationId: string
+}
 
-export const getServerSideProps: GetServerSideProps = withAuth(async context => {
-  const { myOffers, proposal } = fakeData
-
-  // TODO: Remove fake data
-
+export const getServerSideProps = withAuth(async context => {
   const { params, session } = context
-  const { id, offerId } = params as Params
+  const { id, negotiationId } = params as Params
 
-  // if (![proposal.company.id, proposal.offerCompany?.id].includes(session?.user.id)) return { notFound: true }
+  const { data: proposal } = await proposalService.get(id)
+  const { data: negotiation } = await negotiationService.get(negotiationId)
+  const { data: offersData } = await offerService.getCompanyOffers({
+    negotiationId,
+    orderBy: 'createdAt',
+    orderDirection: OrderDirection.Desc,
+  })
 
-  console.log({ id, offerId })
+  const userParticipateInNegotiation =
+    !!session.user.companyId &&
+    [negotiation.companyProponent.id, negotiation.companyInterested.id].includes(session.user.companyId)
+  const userIsICT = checkUserIsICT(session.user)
+
+  if (!userParticipateInNegotiation && !userIsICT) return { notFound: true }
+
+  if (proposal.status === ProposalStatus.onFormalization && !userIsICT) {
+    return {
+      redirect: {
+        destination: `${pages.proposal}/${proposal.id}${pages.formalization}`,
+        permanent: false,
+      },
+    }
+  }
 
   return {
     props: {
-      proposal,
-      offers: myOffers[0],
+      negotiation: {
+        ...negotiation,
+        proposal,
+      },
+      offers: offersData.offers,
       session,
     },
   }
