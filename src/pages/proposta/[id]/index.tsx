b6b0bb27ad8b8f938a984b30624d9c4eef8dc41b
Jander Silva <jander.silv@outlook.com>
2023-06-20 14:57:42 -0300

    ✨ feat(pages/proposata/[id]): fetches data from the api.

--- src/pages/proposta/[id]/index.tsx
+++ src/pages/proposta/[id]/index.tsx
@@ -1,14 +1,14 @@
 import { GetStaticProps, NextPage } from 'next'
-import Head from 'next/head'
+import useSWR, { SWRConfig } from 'swr'
 import { ParsedUrlQuery } from 'querystring'
-import { Box, Theme, useMediaQuery, useScrollTrigger } from '@mui/material'
+import { Box, Theme, Typography, useMediaQuery, useScrollTrigger } from '@mui/material'
 
-import { fakeData } from 'src/data/fake'
 import { checkUserIsCompany, checkUserIsICT } from 'src/helpers/users'
-import { useProposalSession, useTab } from 'src/hooks/proposal'
+import { useProposal, useProposalSession, useTab } from 'src/hooks/proposal'
+import { proposalService } from 'src/services/proposal'
 
-import { ProposalType } from 'src/models/enums'
-import { Offer, Proposal } from 'src/models/types'
+import { ProposalStatus, ProposalType } from 'src/models/enums'
+import { Proposal } from 'src/models/types'
 
 import {
   AsideContentOwner,
@@ -16,26 +16,27 @@ import {
   DefaultAsideContent,
   ICTAsideContent,
   MobileNavigation,
-  OfferCard,
+  NegotiationCard,
   OfferCompanyAsideContent,
+  ProposalCard,
   ProposalSections,
 } from 'src/components/proposal'
 import { ScrollTop, TabPanel } from 'src/components/shared'
 import { Layout } from 'src/layouts/app'
 
-import { BusinessIcon, DescriptionIcon } from 'src/assets/icons'
+import { BusinessIcon, DescriptionIcon, LibraryBooksIcon } from 'src/assets/icons'
 import { ProposalTitle, Section, Tab, Tabs, Wrapper } from 'src/styles/proposal'
+import { pages } from 'src/constants'
 
 type ProposalPageProps = {
-  proposal: Proposal
-  offers: Offer[][]
+  initialProposal: Proposal
 }
 
 const ProposalPage: NextPage<ProposalPageProps> = props => {
-  const { proposal, offers } = props
+  const { initialProposal } = props
+
   const isMobile = useMediaQuery<Theme>(theme => theme.breakpoints.down('md'))
   const scrollTrigger = useScrollTrigger()
-  const { userIsTheProposalOwner, status, session } = useProposalSession(proposal)
   const { tabs, selectedTab, handleChangeTab, a11yTabProps } = useTab([
     {
       label: 'Proposta',
@@ -45,89 +46,120 @@ const ProposalPage: NextPage<ProposalPageProps> = props => {
       label: 'Empresas Interessadas',
       icon: BusinessIcon,
     },
+    ...(!initialProposal.types.includes(ProposalType.research)
+      ? [
+          {
+            label: 'Propostas Similares',
+            icon: LibraryBooksIcon,
+          },
+        ]
+      : []),
   ])
 
-  const documentTitle = `Proposta ${proposal.id} - Connis`
+  const { userIsTheProposalOwner, status, session } = useProposalSession(initialProposal)
+  const { negotiations, similarProposals } = useProposal(initialProposal.id, userIsTheProposalOwner)
+
+  const getProposalUrl = `${proposalService.baseUrl}/${initialProposal.id}`
+  const { data: proposal } = useSWR(getProposalUrl, proposalService.getFetcher, {
+    fallbackData: initialProposal,
+    revalidateOnFocus: false,
+  })
 
   return (
-    <Layout>
-      <Head>
-        <title>{documentTitle}</title>
-      </Head>
+    <Layout documentTitle={`Proposta ${proposal.id}`}>
+      <SWRConfig
+        value={{
+          fallback: {
+            [getProposalUrl]: initialProposal,
+          },
+        }}
+      >
+        <ProposalTitle>{proposal.title}</ProposalTitle>
 
-      <ProposalTitle>{proposal.title}</ProposalTitle>
-
-      {userIsTheProposalOwner && (
-        <Tabs value={selectedTab} onChange={handleChangeTab} aria-label="Abas de Navegação" centered>
-          {tabs.map(({ label }, index) => (
-            <Tab key={label} label={label} {...a11yTabProps(index)} />
-          ))}
-        </Tabs>
-      )}
-
-      <Wrapper maxWidth="xl">
-        <TabPanel value={selectedTab} index={0} flex={1} withGrowAnimation>
-          <ProposalSections proposal={proposal} />
-        </TabPanel>
-        {userIsTheProposalOwner && (
-          <TabPanel value={selectedTab} index={1} flex={1} withGrowAnimation>
-            <Box component="main" maxWidth={750} ml="auto">
-              {offers.map(offerHistory => {
-                const lastOffer = offerHistory.at(-1)
-                return (
-                  lastOffer && (
-                    <OfferCard
-                      key={lastOffer.id}
-                      offer={lastOffer}
-                      href={`/proposta/${lastOffer.proposalId}/oferta/${lastOffer.id}`}
-                      unseenActivities={lastOffer.viewed ? 0 : 1}
-                    />
-                  )
-                )
-              })}
-            </Box>
-          </TabPanel>
+        {userIsTheProposalOwner && proposal.status !== ProposalStatus.onFormalization && (
+          <Tabs value={selectedTab} onChange={handleChangeTab} aria-label="Abas de Navegação" centered>
+            {tabs.map(({ label }, index) => (
+              <Tab key={label} label={label} {...a11yTabProps(index)} />
+            ))}
+          </Tabs>
         )}
 
-        <Box component="aside" flex={userIsTheProposalOwner ? 0.3 : 0.4} position="relative">
-          <Section sx={{ position: 'sticky', top: 32 }}>
-            {(() => {
-              if (status === 'loading') return <CompanyData {...proposal.company} />
-              else if (status === 'unauthenticated') return <DefaultAsideContent {...proposal.company} />
-              else if (
-                proposal.proposalType.includes(ProposalType.research) &&
-                session?.user &&
-                checkUserIsICT(session.user)
-              )
-                return <ICTAsideContent {...proposal.company} />
-              else if (userIsTheProposalOwner) return <AsideContentOwner {...proposal} />
-              else if (session?.user && checkUserIsCompany(session.user))
-                return <OfferCompanyAsideContent proposal={proposal} />
-              else return <CompanyData {...proposal.company} />
-            })()}
-          </Section>
-        </Box>
-      </Wrapper>
+        <Wrapper maxWidth="xl">
+          <TabPanel value={selectedTab} index={0} flex={1} withGrowAnimation>
+            <ProposalSections proposal={proposal} />
+          </TabPanel>
+          {userIsTheProposalOwner && (
+            <TabPanel value={selectedTab} index={1} flex={1} withGrowAnimation>
+              <Box component="main" maxWidth={750} ml="auto">
+                {!!negotiations?.length ? (
+                  negotiations.map(negotiation => (
+                    <NegotiationCard
+                      key={negotiation.id}
+                      negotiation={negotiation}
+                      href={`${pages.proposal}/${proposal.id}${pages.negotiation}/${negotiation.id}`}
+                      unseenActivities={1}
+                      hideTitle
+                    />
+                  ))
+                ) : (
+                  <Typography alignItems="center">
+                    Nenhuma empresa demonstrou interesse na sua proposta até o momento.
+                  </Typography>
+                )}
+              </Box>
+            </TabPanel>
+          )}
+          {userIsTheProposalOwner && !proposal.types.includes(ProposalType.research) && (
+            <TabPanel value={selectedTab} index={2} flex={1} withGrowAnimation>
+              <Box component="main" maxWidth={750} ml="auto">
+                {!!similarProposals?.length ? (
+                  similarProposals.map(similarProposal => (
+                    <ProposalCard key={similarProposal.id} proposal={similarProposal} />
+                  ))
+                ) : (
+                  <Typography alignItems="center">Nenhuma proposta similar foi encontrada.</Typography>
+                )}
+              </Box>
+            </TabPanel>
+          )}
 
-      {userIsTheProposalOwner && (
-        <MobileNavigation
-          tabs={tabs}
-          selectedTab={selectedTab}
-          handleChangeTab={handleChangeTab}
-          scrollTrigger={scrollTrigger}
+          <Box component="aside" flex={userIsTheProposalOwner ? 0.3 : 0.4} position="relative">
+            <Section sx={{ position: 'sticky', top: 32 }}>
+              {(() => {
+                if (status === 'loading') return <CompanyData type="company" {...proposal.company} />
+                else if (status === 'unauthenticated') return <DefaultAsideContent {...proposal.company} />
+                else if (checkUserIsICT(session?.user) && proposal.types.includes(ProposalType.research))
+                  return <ICTAsideContent {...proposal.company} />
+                else if (userIsTheProposalOwner)
+                  return <AsideContentOwner {...proposal} hasOffers={!!negotiations?.length} />
+                else if (checkUserIsCompany(session?.user) && !proposal.types.includes(ProposalType.research))
+                  return <OfferCompanyAsideContent proposal={proposal} />
+                else return <CompanyData type="company" {...proposal.company} />
+              })()}
+            </Section>
+          </Box>
+        </Wrapper>
+
+        {userIsTheProposalOwner && (
+          <MobileNavigation
+            tabs={tabs}
+            selectedTab={selectedTab}
+            handleChangeTab={handleChangeTab}
+            scrollTrigger={scrollTrigger}
+          />
+        )}
+
+        <ScrollTop
+          sx={
+            userIsTheProposalOwner && isMobile
+              ? {
+                  bottom: 70,
+                  transform: scrollTrigger ? 'translateY(50px)' : 'translateY(0)',
+                }
+              : {}
+          }
         />
-      )}
-
-      <ScrollTop
-        sx={
-          userIsTheProposalOwner && isMobile
-            ? {
-                bottom: 70,
-                transform: scrollTrigger ? 'translateY(50px)' : 'translateY(0)',
-              }
-            : {}
-        }
-      />
+      </SWRConfig>
     </Layout>
   )
 }
@@ -139,31 +171,38 @@ type Params = {
 } & ParsedUrlQuery
 
 export const getStaticPaths = async () => {
-  const { myProposals } = fakeData
+  const { data: proposals } = await proposalService.list({
+    pageSize: 12,
+  })
 
-  const paths = myProposals.map(proposal => ({
-    params: {
-      id: proposal.id.toString(),
-    },
-  }))
+  const paths = proposals
+    .filter(proposal => (proposal.trl as number) !== 0 && (proposal.goalTrl as number) !== 0)
+    .map(proposal => ({
+      params: {
+        id: proposal.id.toString(),
+      },
+    }))
 
   return {
     paths,
-    fallback: false,
+    fallback: 'blocking',
   }
 }
 
 export const getStaticProps: GetStaticProps = async context => {
-  const { proposal, myOffers } = fakeData
-
   const { id } = context.params as Params
 
-  console.log({ id })
+  try {
+    const { data: proposal } = await proposalService.get(id)
 
-  return {
-    props: {
-      proposal,
-      offers: myOffers,
-    },
+    return {
+      props: {
+        initialProposal: proposal,
+      },
+    }
+  } catch (error) {
+    return {
+      notFound: true,
+    }
   }
 }
